c9e91af2227b2aa0818871181823f09ae82310b8
Nazar Rohozhuk <nazarrrka@gmail.com>
2019-04-01 16:31:51 +0300

    update dockerfile && launching

--- server/index.js
+++ server/index.js
@@ -5,10 +5,3 @@ const tgBot = require('./bots/telegram.bot');
 amqplib.connect()
 
 tgBot.init();
-
-eventBus.subscribe('notifyEvent', (data) => {
-    console.log(data);
-
-    return Promise.resolve();
-});
-
