3589c597d2d4d5f3c0b5c136b930678f1af37d8b
kodexxx <nazarrrka@gmail.com>
2019-04-08 11:37:07 +0300

    add one demo test

--- server/api/integration/integration.logic.js
+++ server/api/integration/integration.logic.js
@@ -4,20 +4,23 @@ const _ = require('lodash');
 
 class IntegrationLogic {
     getSupportedEvents() {
-        return _.chain(events)
-            .map((provider, eventName) => ({
-                eventTypes: _.keys(provider),
-                eventName,
-            }))
-            .reduce((accum, { eventTypes, eventName }) => [
-                ...accum,
-                ...eventTypes.map(type => ({ type, eventName })),
-            ], [])
-            .reduce((accum, { type, eventName }) => ({
-                ...accum,
-                [type]: (accum[type] || []).concat(eventName),
-            }), {})
-            .value();
+        return {
+            providers: _.chain(events)
+                .map((provider, eventName) => ({
+                    eventTypes: _.keys(provider),
+                    eventName,
+                }))
+                .reduce((accum, { eventTypes, eventName }) => [
+                    ...accum,
+                    ...eventTypes.map(type => ({ type, eventName })),
+                ], [])
+                .reduce((accum, { type, eventName }) => ({
+                    ...accum,
+                    [type]: (accum[type] || []).concat(eventName),
+                }), {})
+                .value(),
+            events: _.keys(events),
+        };
     }
 }
 
